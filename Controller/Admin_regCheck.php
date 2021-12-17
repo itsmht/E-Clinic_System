daab1a63d8cd7cc994078144e60ce5d3a16b5893
Tanvir1526 <astro.tanvir70@gmail.com>
2021-12-17 22:57:29 +0600

    php

--- Controller/Admin_regCheck.php
+++ Controller/Admin_regCheck.php
@@ -53,7 +53,7 @@
 
 
   if ($Uname != "" && $name != "" && $gender != ""&& $email!= "" && $pass != "" ) {
-	  echo "hello";
+	  //echo "hello";
     $jsonData = file_get_contents('../Model/admindata.json');
     $array_data = json_decode($jsonData, true);
     $input = array(
@@ -73,46 +73,51 @@
     file_put_contents('../Model/admindata.json', $final_data);
     header("Location:Admin_login.php");
     echo $final_data;
+  
+    $serverName="localhost";
+    $userName="root";
+    $password="";
+    $dbName="e_clinic";
+    
+    $conn=new mysqli($serverName,$userName,$password,$dbName);
+    if ($conn->connect_error)
+    {
+        die("Conncetion failed: ".$conn->connect_error);
+    }
+    else
+    {
+      /*
+        echo "Successfully connection Created"."<br>";
+        $q="CREATE TABLE Admin (id INT (4) AUTO_INCREMENT PRIMARY KEY,uname VARCHAR(30) NOT NULL,name VARCHAR(30) NOT NULL,email VARCHAR(30) NOT NULL,dob DATENOT NULL,Gender VARCHAR(30) NOT NULL, Address VARCHAR(100) NOT NULL,Password VARCHAR(30) NOT NULL)";
+        $result=$conn->query($q);
+        if($result)
+            {
+                echo"Table Succesfully created ";
+            }
+             
+         else
+            {    
+                echo "Error occured";
+            }
+            */
+        //echo "Successfully connection created!"."<br/>";
+    //$q="INSERT INTO teacher(id,name,email)VALUES(".$id.",".$name.",".$email.")";
+    
+        $q="INSERT INTO RegisterAdmin (username,name,email,dob,gender,address,password) VALUES('".$Uname."','".$name."','".$email."','".$dob."','".$gender."','".$address."','".$pass."')";
+    //echo $q;
+        $result=$conn->query($q);
+        if($result)
+            echo " successfully inserted";
+        else
+            echo "Error occured!";
+    }
+    $conn->close();
+  
+  
   }
   //Database connection
 
-  $serverName="localhost";
-  $userName="root";
-  $password="";
-  $dbName="e_clinic";
   
-  $conn=new mysqli($serverName,$userName,$password,$dbName);
-  if ($conn->connect_error)
-  {
-      die("Conncetion failed: ".$conn->connect_error);
-  }
-  else
-  {
-      echo "Successfully connection Created"."<br>";
-      $q="CREATE TABLE Admin (id INT (4) AUTO_INCREMENT PRIMARY KEY,uname VARCHAR(30) NOT NULL,name VARCHAR(30) NOT NULL,email VARCHAR(30) NOT NULL,dob DATENOT NULL,Gender VARCHAR(30) NOT NULL, Address VARCHAR(100) NOT NULL,Password VARCHAR(30) NOT NULL)";
-      $result=$conn->query($q);
-      if($result)
-          {
-              echo"Table Succesfully created ";
-          }
-           
-       else
-          {    
-              echo "Error occured";
-          }
-          /*
-      echo "Successfully connection created!"."<br/>";
-  //$q="INSERT INTO teacher(id,name,email)VALUES(".$id.",".$name.",".$email.")";
-  
-      $q="INSERT INTO Teacher(name,email) VALUES('".$name."','".$email."')";
-  //echo $q;
-      $result=$conn->query($q);
-      if($result)
-          echo " successfully inserted";
-      else
-          echo "Error occured!";
-  }*/
-  $conn->close();
 
 
 
